e70783fcc34d2e2dde4c87713386136a66d33584
binoy.j.cherian <binoy.j.cherian@PDC1-2HM98BS.dir.svc.accenture.com>
2015-12-28 12:21:55 +0530

    to return a future object of motor Insurance type

---
+++ JInventory/src/threads/CallableMI.java
@@ -0,0 +1,22 @@
+package threads;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import core.InventoryImpl;
+import core.InventoryInterface;
+import plainObjects.MotorInsurance;
+
+public class CallableMI implements Callable<List<MotorInsurance>>{
+
+	InventoryInterface loader=new InventoryImpl();
+	List<MotorInsurance> insuranceList=new ArrayList<>();
+	@Override
+	public List<MotorInsurance> call() throws Exception {
+		
+		insuranceList=loader.readMotorInsuranceFromCSV();
+		return insuranceList;
+	}
+
+}
